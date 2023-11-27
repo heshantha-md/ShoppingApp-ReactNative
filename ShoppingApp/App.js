7fb7751d6091481666214cf5c5370b73c28696d6
Heshantha Mahawaththage Don <heshantha.don@icloud.com>
2023-11-27 11:17:42 +0000

    Implementing shopping cart with react navigation stack - Complete

--- ShoppingApp/App.js
+++ ShoppingApp/App.js
@@ -1,6 +1,6 @@
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { createStackNavigator } from '@react-navigation/stack';
 import { Provider } from 'react-redux';
 
 import { CategoriesView } from './views/CategoriesView';
@@ -9,7 +9,7 @@ import { ItemDetailsView } from './views/ItemDetailsView';
 import { CartView } from './views/CartView';
 import { store } from './store/store';
 
-const stack = createNativeStackNavigator();
+const stack = createStackNavigator();
 
 export default function App() {
   return (
@@ -23,12 +23,16 @@ export default function App() {
                     contentStyle: { backgroundColor: '#ffffff'}
                   }}
                 >
-                  <stack.Screen name='CategoriesView' component={CategoriesView} options={{
-                      title: 'Home'
-                  }} />
-                  <stack.Screen name='ItemsView' component={ItemsView} />
-                  <stack.Screen name='ItemDetailsView' component={ItemDetailsView} />
-                  <stack.Screen name='CartView' component={CartView} />
+                  <stack.Group>
+                    <stack.Screen name='CategoriesView' component={CategoriesView} options={{
+                        title: 'Home'
+                    }} />
+                    <stack.Screen name='ItemsView' component={ItemsView} />
+                    <stack.Screen name='ItemDetailsView' component={ItemDetailsView} />
+                  </stack.Group>
+                  <stack.Group screenOptions={{ presentation: 'modal' }}>
+                    <stack.Screen name='CartView' component={CartView} />
+                  </stack.Group>
                 </stack.Navigator>
           </NavigationContainer>
       </Provider>
